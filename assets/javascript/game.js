3d2d35375c8eeb5294a609a06626c27974163651
Bibek Subedi <bsubedi26@gmail.com>
2016-04-26 16:30:17 -0400

    Added crystal images and an unique random number was generated for each image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,10 +6,27 @@ $(document).ready(function() {
 		winsLosses: $("#winsLosses"),
 		numGenerate: function (min, max) {
     	return Math.floor(Math.random()*(max-min)+min);
+
 	}	
 		
 	}
 $(randomNumber).html(game.numGenerate(19,121));
-$(winsLosses).html("Wins: " + "<br>" + "Losses: ")	
+$(winsLosses).html("Wins: " + "<br>" + "Losses: ")
+var random1 = Math.floor(Math.random()*(13-1)+1);
+var random2 = Math.floor(Math.random()*(13-1)+1);
+var random3 = Math.floor(Math.random()*(13-1)+1);
+var random4 = Math.floor(Math.random()*(13-1)+1);
+$("#crystal1").on("click", function(){
+	 console.log(random1);
+})
+$("#crystal2").on("click", function(){
+	 console.log(random2);
+})
+$("#crystal3").on("click", function(){
+	 console.log(random3);
+})
+$("#crystal4").on("click", function(){
+	 console.log(random4);
+})	
 
 });
