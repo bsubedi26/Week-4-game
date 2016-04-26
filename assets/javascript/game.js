00d5da88a5d39686874032d9a9fe20c28668a098
Bibek Subedi <bsubedi26@gmail.com>
2016-04-26 17:43:58 -0400

    Added further javascript to get the total score and compare it with the random number and increment wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,29 +4,66 @@ $(document).ready(function() {
 	var game = {
 		randomNumber: $("#randomNumber"),
 		winsLosses: $("#winsLosses"),
+		totalscore: $("#totalscore"),
 		numGenerate: function (min, max) {
     	return Math.floor(Math.random()*(max-min)+min);
 
-	}	
-		
+	},	
+
 	}
-$(randomNumber).html(game.numGenerate(19,121));
-$(winsLosses).html("Wins: " + "<br>" + "Losses: ")
+var randomGeneratedNumber = game.numGenerate(19,121);
+$(randomNumber).html(randomGeneratedNumber);
 var random1 = Math.floor(Math.random()*(13-1)+1);
 var random2 = Math.floor(Math.random()*(13-1)+1);
 var random3 = Math.floor(Math.random()*(13-1)+1);
 var random4 = Math.floor(Math.random()*(13-1)+1);
+var total = 0;
+var wins = 0;
+var losses = 0;
+$(totalscore).html("Your total score is: " + total)
+$(winsLosses).html("Wins: " + wins + "<br>" + "Losses: " + losses)
+
+
 $("#crystal1").on("click", function(){
-	 console.log(random1);
+	 total += random1;
+	 console.log(total);
+	 $(totalscore).html("Your total score is: " + total)
+	 	if (total == randomGeneratedNumber) {
+			wins++;
+			location.reload();
+			}
 })
 $("#crystal2").on("click", function(){
-	 console.log(random2);
+	 total += random2;
+	 console.log(total);
+	 $(totalscore).html("Your total score is: " + total)
+	 if (total == randomGeneratedNumber) {
+			wins++;
+			location.reload();
+		}
 })
 $("#crystal3").on("click", function(){
-	 console.log(random3);
+	 total += random3;
+	 console.log(total);
+	 $(totalscore).html("Your total score is: " + total)
+	 if (total == randomGeneratedNumber) {
+			wins++;
+			location.reload();
+		}
 })
 $("#crystal4").on("click", function(){
-	 console.log(random4);
-})	
+	 total += random4;
+	 console.log(total);
+	 $(totalscore).html("Your total score is: " + total)
+	 if (total == randomGeneratedNumber) {
+			wins++;
+			location.reload();
+		}
+})
+
+
+	
+
+
 
 });
